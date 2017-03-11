d6d82b8223817bb79b4fe212fce138ebecb1e554
Kristin Greenslit <kristingreenslit@gmail.com>
2017-03-10 21:30:47 -0500

    change file and class names

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import '../styles/App.css';
+import '../styles/app.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
+      <div className="app">
+        <div className="app-header"></div>
+        <p className="app-intro">
           hello world!
         </p>
+        <div className="app-footer"></div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/app';
 import './styles/index.css';
 
 ReactDOM.render(
