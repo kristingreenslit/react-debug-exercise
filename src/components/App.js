c85740989d3634f0aa20ef12c11ed4c3487d4737
Kristin Greenslit <kristingreenslit@gmail.com>
2017-03-10 21:18:32 -0500

    update file structure

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          hello world!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
