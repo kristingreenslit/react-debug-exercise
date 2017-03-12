b0650d48a841407e89605360b93902c1265c78f5
Kristin Greenslit <kristingreenslit@gmail.com>
2017-03-12 16:03:32 -0400

    add sign out button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,11 +3,13 @@ import '../styles/App.css';
 
 class Navbar extends Component {
   render() {
+    console.log(this);
     return (
       <div className='app-navbar'>
       	<div className='flex-container'>
 	      	<div className='header'>React Debug App</div>
-	      	<button className='flat-button border-white' onClick={this.props.handleLogin}>Log In</button>
+	      	<button className='flat-button border-white' onClick={this.props.handleLogin}>Sign In</button>
+          <button className='flat-button border-white' onClick={this.props.handleLogout}>Sign Out</button>
         </div>
       </div>
     );
--- src/containers/App.js
+++ src/containers/App.js
@@ -9,23 +9,44 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { showLoginForm: false };
+    this.state = {
+      showLoginButton: true,
+      showLogoutButton: false,
+      showLoginForm: false
+    };
     this.handleLogin = this.handleLogin.bind(this);
     this.handleCredentials = this.handleCredentials.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
   }
 
   handleLogin() {
-    this.setState({ showLoginForm: true });
+    this.setState({
+      showLoginButton: false,
+      showLogoutButton: true,
+      showLoginForm: true
+    });
   }
 
   handleCredentials() {
-    this.setState({ showLoginForm: false });
+    this.setState({
+      showLoginButton: false,
+      showLogoutButton: true,
+      showLoginForm: false
+    });
+  }
+
+  handleLogout() {
+    this.setState({
+      showLoginButton: true,
+      showLogoutButton: false,
+      showLoginForm: true
+    });
   }
 
   render() {
     return (
       <div className='app'>
-        <Navbar handleLogin={this.handleLogin} />
+        <Navbar handleLogin={this.handleLogin} handleLogout={this.handleLogout} />
         <div className={this.state.showLoginForm === true ? '' : 'hide'}>
           <LoginForm handleCredentials={this.handleCredentials} />
         </div>
